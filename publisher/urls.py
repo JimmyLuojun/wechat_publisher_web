a974d260605d43a55c94ca5cbcdb00ae5b133918
Jimmy <15828061015@163.com>
2025-05-07 21:29:14 +0800

    we can navigate to http://127.0.0.1:8000/ instead of http://127.0.0.1:8000/publisher/upload/ to meet the main function of the project.

--- publisher/urls.py
+++ publisher/urls.py
@@ -1,30 +1,19 @@
 # /Users/junluo/Documents/wechat_publisher_web/publisher/urls.py
 """
 URL configuration for the publisher app.
-
-Maps URL paths to their corresponding API views.
 """
 from django.urls import path
 from . import views # Import views from the current app
 
-app_name = 'publisher' # Namespace for URLs (optional but recommended)
+app_name = 'publisher' # Namespace for URLs
 
 urlpatterns = [
-    # URL for the HTML form page - CHECK THIS LINE CAREFULLY
-    path('upload/', views.UploadFormView.as_view(), name='upload_form'), # Make sure name='upload_form' is exact
+    # This maps the root path (relative to where these URLs are included)
+    # to your UploadFormView. Since the main urls.py includes this at "",
+    # this view will handle requests to the site's root (e.g., http://127.0.0.1:8000/).
+    path('', views.UploadFormView.as_view(), name='upload_form'),
 
-    # URLs for the API endpoints
+    # These API endpoints will be accessible at /api/process/ and /api/confirm/
     path('api/process/', views.ProcessPreviewAPIView.as_view(), name='process_preview_api'),
     path('api/confirm/', views.ConfirmPublishAPIView.as_view(), name='confirm_publish_api'),
 ]
-
-# Note: You'll also need to include these URLs in your main project's urls.py
-# Example in wechat_publisher_web/urls.py:
-# from django.urls import path, include
-#
-# urlpatterns = [
-#     path('admin/', admin.site.urls),
-#     path('publisher/', include('publisher.urls', namespace='publisher')), # Include app URLs
-#     # ... other project urls
-# ]
-# Also configure MEDIA_URL serving in project urls.py for development previews.
--- wechat_publisher_web/urls.py
+++ wechat_publisher_web/urls.py
@@ -1,31 +1,19 @@
 # /Users/junluo/Documents/wechat_publisher_web/wechat_publisher_web/urls.py
 """
 URL configuration for wechat_publisher_web project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include # Make sure include is imported
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("publisher/", include("publisher.urls", namespace="publisher")),
+    # This includes your publisher app's URLs starting from the root path ""
+    path("", include("publisher.urls", namespace="publisher")),
 ]
 
+# This part is for serving static and media files during development
 if settings.DEBUG:
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
